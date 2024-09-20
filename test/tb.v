6e2b6a31b5f32f9719015f8b1f5f7e3d5adb853e
htfab <mpw@htamas.net>
2024-09-20 04:59:04 +0200

    fix: VPWR & VGND need to support continuous assignment

--- test/tb.v
+++ test/tb.v
@@ -28,6 +28,10 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
 
   wire [3:0] led = uo_out[3:0];
   wire speaker = uo_out[4];
@@ -39,8 +43,8 @@ module tb ();
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
+      .VPWR(VPWR),
+      .VGND(VGND),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
